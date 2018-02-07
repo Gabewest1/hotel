6501adcda769c2ccd1e0336cc75d00b3da45298e
Gabriel West <gabe_west1@yahoo.com>
2018-02-07 05:11:04 -0600

    Created listing data that is translated into <Listing/> components.

---
+++ src/pages/Listings/data.js
@@ -0,0 +1,29 @@
+const listings = [
+    {
+        description: "Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag. Photo booth migas coloring book pug thundercats next level +1 pop-up.",
+        src: "/assets/images/filler.jpg",
+        title: "Lorem Title Ipsum"
+    },
+    {
+        description: "Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag. Photo booth migas coloring book pug thundercats next level +1 pop-up.",
+        src: "/assets/images/filler.jpg",
+        title: "Lorem Title Ipsum"
+    },
+    {
+        description: "Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag. Photo booth migas coloring book pug thundercats next level +1 pop-up.",
+        src: "/assets/images/filler.jpg",
+        title: "Lorem Title Ipsum"
+    },
+    {
+        description: "Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag. Photo booth migas coloring book pug thundercats next level +1 pop-up.",
+        src: "/assets/images/filler.jpg",
+        title: "Lorem Title Ipsum"
+    },
+    {
+        description: "Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag. Photo booth migas coloring book pug thundercats next level +1 pop-up.",
+        src: "/assets/images/filler.jpg",
+        title: "Lorem Title Ipsum"
+    }
+]
+
+export default listings
