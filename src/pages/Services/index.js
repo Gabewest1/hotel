f3daeef92e29cc75bda524a2f8aa0e86906fbb41
Gabriel West <gabe_west1@yahoo.com>
2018-02-06 23:03:49 -0600

    Added filler markup to the <Services/> page.

--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -5,12 +5,63 @@ class Services extends Component {
   render() {
     return (
       <ServicesView>
-        AYYy LmaO ItS THe Services PAGE
+        <Title>Services Page</Title>
+
+        <Section>
+          <Title>Food</Title>
+          <ImageContainer>
+            <Image src="/assets/images/filler.jpg"/>
+          </ImageContainer>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque natus qui tempore iste doloremque dignissimos temporibus dolor modi, reiciendis animi tenetur eveniet facere expedita dicta dolorem. Quasi soluta iusto ea.
+          </Description>
+        </Section>
+
+        <Section>
+          <Title>Gym</Title>
+          <ImageContainer>
+            <Image src="/assets/images/filler.jpg"/>
+          </ImageContainer>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque natus qui tempore iste doloremque dignissimos temporibus dolor modi, reiciendis animi tenetur eveniet facere expedita dicta dolorem. Quasi soluta iusto ea.
+          </Description>
+        </Section>
+
+        <Section>
+          <Title>Pool</Title>
+          <ImageContainer>
+            <Image src="/assets/images/filler.jpg"/>
+          </ImageContainer>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque natus qui tempore iste doloremque dignissimos temporibus dolor modi, reiciendis animi tenetur eveniet facere expedita dicta dolorem. Quasi soluta iusto ea.
+          </Description>
+        </Section>
+
+        <Section>
+          <Title>Bar</Title>
+          <ImageContainer>
+            <Image src="/assets/images/filler.jpg"/>
+          </ImageContainer>
+          <Description>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque natus qui tempore iste doloremque dignissimos temporibus dolor modi, reiciendis animi tenetur eveniet facere expedita dicta dolorem. Quasi soluta iusto ea.
+          </Description>
+        </Section>
       </ServicesView>
     )
   }
 }
 
+const Title = styled.h1`
+
+`
+const Description = styled.p`
+
+`
+const Image = styled.img``
+const ImageContainer = styled.div``
+const Section = styled.section`
+
+`
 const ServicesView = styled.div`
 
 `
