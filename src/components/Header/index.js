2a7b0a70c848df74c298923a2e4f82b725edafb1
Gabriel West <gabe_west1@yahoo.com>
2018-02-08 21:34:04 -0600

    Updated Header: Wrapped title and description in a div with a white background.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,12 +5,14 @@ class Header extends React.Component {
     render() {
         return (
             <HeaderView>
-                <Title>
-                    Lorem Title Ipsum
-                </Title>
-                <Description>
-                    Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag.
-                </Description>
+                <Wrapper>
+                    <Title>
+                        Lorem Title Ipsum
+                    </Title>
+                    <Description>
+                        Lorem ipsum dolor amet poutine humblebrag kale chips, health goth flexitarian cray hashtag. 8-bit cornhole photo booth direct trade selfies normcore, cold-pressed salvia. Locavore godard food truck, heirloom pork belly coloring book wayfarers mixtape asymmetrical try-hard pabst lyft ethical swag.
+                    </Description>
+                </Wrapper>
             </HeaderView>
         )
     }
@@ -18,26 +20,32 @@ class Header extends React.Component {
 
 const SPACING = `
     position: relative;
-    top: 75px;
+    top: 275px;
     left: 75px;
     margin: 0;
 `
-const Title = styled.h1`
+const Wrapper = styled.div`
     ${ SPACING };
+    background: #efefefcc;
+    display: inline-block;
+    text-align: center;
+    padding: 15px;
+    border-radius: 9px;
+`
+const Title = styled.h1`
     font-size: 72px;
     font-family: 'Archivo Black', sans-serif;    
+    margin: 0;
     margin-bottom: 15px;
 `
 const Description = styled.p`
-    ${ SPACING };
     max-width: 768px;
-    font-family: 'Raleway', sans-serif;
     font-size: 18px;
 `
 const HeaderView = styled.div`
     color: #161921;
     height: 100vh;
-    background: url(/assets/images/home_hero.jpg) no-repeat;
+    background: url(/assets/images/home_hero2.jpg) no-repeat;
     background-size: cover;
     background-position: 80% center;
 `
