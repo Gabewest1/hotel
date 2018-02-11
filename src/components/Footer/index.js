6cbb7545253cb22dfc67c1d3a39926ac72390703
Gabriel West <gabe_west1@yahoo.com>
2018-02-10 23:15:27 -0600

    Added <LineAbove/> to the footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,31 +9,31 @@ class Footer extends React.Component {
         return (
             <FooterView>
                 <ItemsContainer>
-                    <_Link to="/">
-                        <Logo src="/assets/images/logo5alt.svg" />
-                    </_Link>
+                    <LogoWrapper>
+                        <_Link to="/">
+                            <Logo src="/assets/images/logo5alt.svg" />
+                        </_Link>
+                        <SocialMediaView />                    
+                    </LogoWrapper>
                     <Links>
+
+                        <LineAbove />
+
                         <List>
                             <ListItem><Link to="">Contact Us</Link></ListItem>
                             <ListItem><Link to="">Meetings &amp; Events</Link></ListItem>
                             <ListItem><Link to="">Rooms &amp; Suites</Link></ListItem>
                             <ListItem><Link to="">Rooms</Link></ListItem>
                         </List>
-                        <Line>
-                        </Line>
+
+                        <Line />
+
                         <Copyright>© Hotel Concepts 2018</Copyright>
                     </Links>
-                    <SocialMedia>
-                        <Icon>
-                            <Logo src="/assets/images/facebook.svg" />
-                        </Icon>
-                        <Icon>
-                            <Logo src="/assets/images/twitter.svg" />
-                        </Icon>
-                            <Logo src="/assets/images/Instagram.svg" />
-                        <Icon>
-                        </Icon>
-                    </SocialMedia>
+                    
+                    <HideOnMobile>
+                        <SocialMediaView />
+                    </HideOnMobile>
                 </ItemsContainer>
             </FooterView>
         )
@@ -45,56 +45,16 @@ const Icon = styled.a`
 `
 
 const Copyright = styled.div`
-// background-color: green;
     width: 100%;
     position: relative;
     top: 65px;
     color: #635c45;    
     font-size: 14px;
     letter-spacing: 1px;
-    font-family: 'Rammetto One', cursive;
-    font-family: 'Open Sans', sans-serif;
-    font-family: 'Lato', sans-serif;
-    font-family: 'Roboto Condensed', sans-serif;
-    // font-family: 'Montserrat', sans-serif;
-    font-family: 'Oswald', sans-serif;
-    // font-family: 'Slabo 27px', serif;
-    // font-family: 'Source Sans Pro', sans-serif;
-    // font-family: 'Dhurjati', sans-serif;
-    // font-family: 'Raleway', sans-serif;
-    // font-family: 'PT Sans', sans-serif;
-    // font-family: 'Roboto Slab', serif;
-    // font-family: 'Merriweather', serif;
-    // font-family: 'Open Sans Condensed', sans-serif;
-    // font-family: 'Lora', serif;
-    // font-family: 'Ubuntu', sans-serif;
-    // font-family: 'Playfair Display', serif;
-    // font-family: 'Noto Sans', sans-serif;
-    // font-family: 'Poppins', sans-serif;
-    // font-family: 'PT Serif', serif;
-    // font-family: 'Titillium Web', sans-serif;
-    // font-family: 'Arimo', sans-serif;
-    // font-family: 'Muli', sans-serif;
-    // font-family: 'PT Sans Narrow', sans-serif;
-    // font-family: 'Encode Sans', sans-serif;
-    // font-family: 'Noto Serif', serif;
-    // font-family: 'Indie Flower', cursive;
-    // font-family: 'Dosis', sans-serif;
-    // font-family: 'Nunito', sans-serif;
     font-family: 'Anton', sans-serif;
-    // font-family: 'Inconsolata', monospace;
-    // font-family: 'Crimson Text', serif;
-    // font-family: 'Bitter', serif;
-    // font-family: 'Fira Sans', sans-serif;
-    // font-family: 'Alegreya Sans', sans-serif;
-    // font-family: 'Archivo', sans-serif;
-    // font-family: 'Oxygen', sans-serif;
-    // font-family: 'Josefin Sans', sans-serif;
-    // font-family: 'Libre Baskerville', serif;
 `
 
 const List = styled.ul`
-// background-color: red;
     font-family: 'Montserrat', sans-serif; 
     text-transform: uppercase;
     font-size: 13px;
@@ -105,10 +65,15 @@ const List = styled.ul`
     width: 80%;
     height: 50%;
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
     align-items: center;
     position: relative;
     bottom: 10px;
+
+    @media (min-width: 1024px) {
+        flex-direction: row;
+    }
 ` 
     
 const ListItem = styled.li`
@@ -126,14 +91,17 @@ const Line = styled.div`
     position: relative;
     top: 10px;
 `
-
+const LineAbove = styled(Line)`
+    @media (min-width: 1024px) {
+        display: none;
+    }
+`
 
 const Logo = styled.img`
     max-width: 250px;
 `
 
 const _Link = styled(Link)`
-// background-color: yellow;
     width: 100%;
     display: flex;
     justify-content: center;
@@ -151,7 +119,6 @@ const Links = styled.div`
     flex: 0 0 52%;   
 `
 const SocialMedia = styled.div`
-// background-color: blue;
     width: 100%;
     display: flex;
     justify-content: space-between;
@@ -161,18 +128,31 @@ const SocialMedia = styled.div`
 `
 
 const ItemsContainer = styled.div`
-// background-color: purple;
     height: 50%;
     width: 88%;
     max-width: 1200px;
     display: flex;
-    // flex-direction: column;
     align-items: center;
     justify-content: center;
     position: relative;
     bottom: 10px;
 `
+const HideOnMobile = styled.div`
+    display: none;
 
+    @media (min-width: 1024px) {
+        display: block;
+    }
+`
+const LogoWrapper = styled.div`
+    text-align: center;
+
+    @media (min-width: 1024px) {
+        ${ SocialMedia } {
+            display: none;
+        }
+    }
+`
 const FooterView = styled.div` 
     color: white;
     width: 100%;
@@ -183,5 +163,18 @@ const FooterView = styled.div`
     align-items: center;
     flex-direction: row;
 `
+const SocialMediaView = () => (
+    <SocialMedia>
+        <Icon>
+            <Logo src="/assets/images/facebook.svg" />
+        </Icon>
+        <Icon>
+            <Logo src="/assets/images/twitter.svg" />
+        </Icon>
+            <Logo src="/assets/images/Instagram.svg" />
+        <Icon>
+        </Icon>
+    </SocialMedia>
+)
 
 export default Footer
