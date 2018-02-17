a1b8ec5695b0b75b47dfd1495eaefb26af814379
Gabriel West <gabe_west1@yahoo.com>
2018-02-17 02:36:52 -0600

    Updated Room Section to use the 4 recently added room images. Included a size and description section in the <Room/>

--- src/pages/Home/RoomsSection.js
+++ src/pages/Home/RoomsSection.js
@@ -8,33 +8,42 @@ export default class RoomsSection extends React.Component {
                 <SectionTitle>Rooms &amp; Suites</SectionTitle>
                 <Row>
                     <RoomView
-                        src="/assets/images/filler.jpg"
-                        title="Junior Suite"
+                        src="/assets/images/rooms/room1.jpg"
+                        title="Executive Suite"
+                        size="800 SQ FT | 75 SQ M"
+                        description={`With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath`}
                     />
                     <RoomView
-                        src="/assets/images/filler.jpg"
-                        title="Deluxe Suite" 
+                        src="/assets/images/rooms/room2.jpg"
+                        title="Deluxe Suite"
+                        size="1000 SQ FT | 93 SQ M"
+                        description={`With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath`}                        
                     />
                 </Row>
                 <Row>
                     <RoomView
-                        src="/assets/images/filler.jpg"
+                        src="/assets/images/rooms/room3.jpg"
                         title="Club Level Suite"
+                        size="1000 SQ FT | 93 SQ M"
+                        description={`With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath`}                        
                     />
                     <RoomView
-                        src="/assets/images/filler.jpg"
+                        src="/assets/images/rooms/room4.jpg"
                         title="Luxury Suite"
+                        size="1475 SQ FT | 138 SQ M"
+                        description={`With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath`}                        
                     />
                 </Row>
           </RoomsSectionView>
         )
     }
 }
+
 const Title = styled.h3`
     margin: 0;
     font-weight: 500;
-    font-family: 'Montserrat',sans-serif;
     margin-bottom: 15px;
+    text-transform: uppercase;
 `
 const SectionTitle = styled.h1`
     margin: 60px auto;
@@ -50,27 +59,43 @@ const SectionTitle = styled.h1`
 const Description = styled.p`
     font-family: montserrat, sans-serif;
     margin: 0;
+    font-size: 12px;
+    letter-spacing: 3,px;
+    margin-top: 14px;
 `
 const Image = styled.img`
     max-width: 100%;
     width: 100%;
 `
 const ImageContainer = styled.div``
+const Size = styled.div`
+    span {
+        margin-right: 10px;
+        text-transform: uppercase;
+        font-weight: bolder;
+    }
+`
+const Details = styled.div`
+    padding: 15px;
+`
 const Room = styled.div`
     min-width: 300px;
     flex-basis: 40%;
     margin-bottom: 40px;
+    box-shadow: 0 0 3px 3px lightgray;
+    font-family: 'Montserrat',sans-serif;    
 `
-
-const RoomView = props => (
+const RoomView = ({ description, size, src, title }) => (
     <Room>
         <ImageContainer>
-            <Image src={ props.src } />
+            <Image src={ src } />
         </ImageContainer>
-        <Title>Delux Suite</Title>
-        <Description>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, maxime itaque. Dolorem veniam veritatis eum consectetur molestias non saepe, perspiciatis odio suscipit ipsam tempore omnis quaerat iste maiores eligendi. Facilis.
-        </Description>
+        
+        <Details>
+            <Title>{ title }</Title>
+            <Size><span>Size</span>{ size }</Size>
+            <Description>{ description }</Description>
+        </Details>
     </Room>
 )
 const Row = styled.div`
