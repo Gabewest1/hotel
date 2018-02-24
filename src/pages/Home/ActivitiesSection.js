5465deb9ab253b49d90befcd486805245e8fb698
Gabriel West <gabe_west1@yahoo.com>
2018-02-23 19:04:52 -0600

    Updated <ActivitiesSection/> to incoude an opac box with the title and a link to show more information.

--- src/pages/Home/ActivitiesSection.js
+++ src/pages/Home/ActivitiesSection.js
@@ -12,41 +12,85 @@ export default class Activities extends React.Component {
 
                 <Wrapper>
                     <ActivityView 
-                        src="/assets/images/activites/snowboarding.jpg"
+                        src="/assets/images/activities/snowboarding.jpg"
                         description="With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath"
+                        title="Winter Adventures"
                     />
 
                     <ActivityView 
-                        src="/assets/images/activites/games.jpg"
+                        src="/assets/images/activities/games.jpg"
                         description="With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath"
+                        title="Indoor Entertainment"
                     />
 
                     <ActivityView 
-                        src="/assets/images/activites/pool.jpg"
+                        src="/assets/images/activities/pool.jpg"
                         description="With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath"
+                        title="Pools & Spas"
                     />
 
                     <ActivityView 
-                        src="/assets/images/activites/gym.jpg"
+                        src="/assets/images/activities/gym.jpg"
                         description="With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath"
+                        title="Fitness"
                     />
                 </Wrapper>
           </ActivitiesView>
         )
     }
 }
-const ActivityView = ({ description, src }) => (
+const ActivityView = ({ description, src, title }) => (
     <Activity>
-        <ImageContainer>
+        <ImageContainer >
             <Image src={ src } />
+            <LearnMoreView title={ title } />
         </ImageContainer>
         <Center>
             <Description>{ description }</Description>
         </Center>
     </Activity>
 )
-const Title = styled.h1`
-
+const LearnMoreView = ({ title }) => (
+    <LearnMoreContainer>
+        <FancyBackground>
+            <Title>{ title }</Title>
+            <LearnMoreText>Learn More</LearnMoreText>
+        </FancyBackground>
+    </LearnMoreContainer>
+)
+const Title = styled.p`
+    margin: 0;
+`
+const LearnMoreText = styled.a`
+    margin: 0;
+    font-size: 10px;
+    margin-top: 5px;
+`
+const FancyBackground = styled.div`
+    background: 
+        url(/assets/images/activities/box-bg-left.png) no-repeat left -1px,
+        url(/assets/images/activities/box-bg-right.png) no-repeat right -1px;
+    box-sizing: border-box;
+    border-top: solid thin ${ PRIMARY_COLOR };
+    border-bottom: solid thin ${ PRIMARY_COLOR };
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: ${ PRIMARY_COLOR };
+    width: 90%;
+    height: 64px;
+`
+const LearnMoreContainer = styled.div`
+    background-color: rgba(0, 0, 0, .8);
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    transform: translateY(-115%);
+    text-transform: uppercase;    
 `
 const Description = styled.p`
     display: flex;
@@ -57,6 +101,7 @@ const Image = styled.img`
 `
 const ImageContainer = styled.div`
     width: 100vw;
+    position: relative;
 `
 const Activity = styled.div`
     display: flex;
