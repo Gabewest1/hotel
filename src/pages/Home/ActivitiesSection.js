62e9e91ef26d3b605f064cd5616f1f825b70eb15
Gabriel West <gabe_west1@yahoo.com>
2018-02-23 22:16:27 -0600

    Updated <ActivityView/> to be responsive.

--- src/pages/Home/ActivitiesSection.js
+++ src/pages/Home/ActivitiesSection.js
@@ -26,7 +26,7 @@ export default class Activities extends React.Component {
                     <ActivityView 
                         src="/assets/images/activities/pool.jpg"
                         description="With a large bedroom, separate living room, balcony and spa-like bathroom, these elegant suites feature a spaious layout generouus seating and a desk. The bathroom boasts a glass enclosed shower and deep soaking bath"
-                        title="Pools & Spas"
+                        title="Pools &amp; Spas"
                     />
 
                     <ActivityView 
@@ -39,58 +39,78 @@ export default class Activities extends React.Component {
         )
     }
 }
+
+const FIRST_BREAKPOINT = "768px"
+
+/*VIEWS*/
 const ActivityView = ({ description, src, title }) => (
     <Activity>
-        <ImageContainer >
-            <Image src={ src } />
-            <LearnMoreView title={ title } />
-        </ImageContainer>
-        <Center>
-            <Description>{ description }</Description>
-        </Center>
+        <AnimatingBox>
+            <ImageContainer >
+                <Image src={ src } />
+                <LearnMoreView title={ title } />
+            </ImageContainer>
+        </AnimatingBox>
     </Activity>
 )
 const LearnMoreView = ({ title }) => (
     <LearnMoreContainer>
         <FancyBackground>
-            <Title>{ title }</Title>
-            <LearnMoreText>Learn More</LearnMoreText>
+            <LearnMoreTitle>{ title }</LearnMoreTitle>
+            <LearnMoreLink>Learn More</LearnMoreLink>
         </FancyBackground>
     </LearnMoreContainer>
 )
-const Title = styled.p`
+
+/* STYLES */
+const LearnMoreTitle = styled.p`
     margin: 0;
+    white-space: nowrap;
+    letter-spacing: 1.8px;
+
+    @media(min-width: ${ FIRST_BREAKPOINT }) {
+        font-size: 18px;
+    }
 `
-const LearnMoreText = styled.a`
+const LearnMoreLink = styled.a`
     margin: 0;
     font-size: 10px;
     margin-top: 5px;
 `
 const FancyBackground = styled.div`
-    background: 
-        url(/assets/images/activities/box-bg-left.png) no-repeat left -1px,
-        url(/assets/images/activities/box-bg-right.png) no-repeat right -1px;
-    box-sizing: border-box;
-    border-top: solid thin ${ PRIMARY_COLOR };
-    border-bottom: solid thin ${ PRIMARY_COLOR };
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     color: ${ PRIMARY_COLOR };
     width: 90%;
-    height: 64px;
+    
+    @media (min-width: 480px) {
+        height: 64px;
+        background: 
+        url(/assets/images/activities/box-bg-left.png) no-repeat left -1px,
+        url(/assets/images/activities/box-bg-right.png) no-repeat right -1px;
+        box-sizing: border-box;
+        border-top: solid thin ${ PRIMARY_COLOR };
+        border-bottom: solid thin ${ PRIMARY_COLOR };
+    }
 `
 const LearnMoreContainer = styled.div`
     background-color: rgba(0, 0, 0, .8);
     box-sizing: border-box;
-    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 8px;
-    transform: translateY(-115%);
     text-transform: uppercase;    
+    position: absolute;
+    bottom: 4px;
+    left: 0;
+    right: 0;
+
+    @media (min-width: ${ FIRST_BREAKPOINT }) {
+        bottom: 15px;
+    }
 `
 const Description = styled.p`
     display: flex;
@@ -98,59 +118,57 @@ const Description = styled.p`
 `
 const Image = styled.img`
     max-width: 100%;
+    width: 100%;
 `
 const ImageContainer = styled.div`
-    width: 100vw;
     position: relative;
+    background: ${ SECONDARY_COLOR };
+    padding: 4px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+`
+const AnimatingBox = styled.div`
+    background-color: ${ PRIMARY_COLOR };
+    padding: 4px;
+    box-sizing: border-box;
+    border: solid thin ${ SECONDARY_COLOR };
+    width: 100%;
+
+    &, > * {
+        transition: padding .5s linear;
+    }
+
+    &:hover, > *:hover {
+        padding: 0;
+    }
+
 `
 const Activity = styled.div`
+    max-width: 700px;
+    width: 100%;
+    height: 60vw;
+
+    @media (min-width: ${ FIRST_BREAKPOINT }) {
+        height: 447px;
+    }
+    @media(min-width: 1200px) {
+        width: 47%;
+    }
+`
+const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
 
-    @media (min-width: 768px) {
-        flex-direction: row;
-        justify-content: space-around;
-        height: 300px;
-
-        ${ Description } {
-            margin-bottom: 0;
-            max-width: 440px;
-        }
-
-        ${ ImageContainer } {
-            width: 40%;
-        }
-    }
-`
-const Center = styled.div`
-    text-align: center;
-
-    @media (min-width: 768px) {
-        width: 40%;
-        display: flex;
-        justify-content: center;
-    }
-`
-const Wrapper = styled.div`
     > * {
         margin-bottom: 70px;
     }
 
-    > *:last-child {
-        margin-bottom: 0px;
-    }
-
-    @media (min-width: 768px) {
-        > * {
-            margin-bottom: 120px;
-        }
-
-        ${ Activity }:nth-child(2n) {
-            flex-direction: row-reverse;
-        }
+    @media (min-width: ${ FIRST_BREAKPOINT }) {
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
     }
 `
-const ActivitiesView = styled.section`
-    padding-bottom: 50px;
-`
+const ActivitiesView = styled.section``
