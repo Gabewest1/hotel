96a5d4f544d6430706f9ded3880f8adf7a9f3cdd
Gabriel West <gabe_west1@yahoo.com>
2018-02-25 20:55:08 -0600

    Set the hero carousel to auto play and loop.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,7 @@ class Home extends Component {
     return (
       <HomeView>
 
-        <Carousel />
+        <Carousel autoPlay infiniteLoop />
 
         <Padding>
           <ContactSection />
